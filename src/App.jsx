9b05744256420124b1186e706c74379090164ff6
hj7321 <oboki2001@naver.com>
2024-05-22 17:29:21 +0900

    수업 종료 후 구현 완료

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,38 @@
 import { useState } from "react";
 
 export default function App() {
-  const [number, setNumber] = useState("");
+  const [number, setNumber] = useState("0");
+  const [newNumber, setNewNumber] = useState("");
 
-  const addNumber = (e) => {
-    console.log(e.target.value);
-    setNumber((prev) => String(+prev + +number));
+  const addNumber = () => {
+    setNumber((prev) => String(+prev + +newNumber));
   };
 
   const subNumber = () => {
-    setNumber((prev) => String(+prev - +number));
+    setNumber((prev) => String(+prev - +newNumber));
   };
 
   const initNumber = () => {
     setNumber((prev) => (prev = "0"));
+    setNewNumber((prev) => (prev = ""));
   };
 
   return (
     <div>
       <h1>덧셈과 뺄셈이 가능한 앱 만들기</h1>
       <div>
-        <input /> 만큼을 <button onClick={addNumber}>더할게요</button>{" "}
+        <input
+          value={newNumber}
+          onChange={(e) => setNewNumber(e.target.value)}
+        />{" "}
+        만큼을 <button onClick={addNumber}>더할게요</button>{" "}
         <button onClick={subNumber}>뺄게요</button>
         <button onClick={initNumber}>초기화</button>
       </div>
       <hr />
       <div>
         <h3>결과</h3>
-        <p>{number === "" ? "0" : number}</p>
+        <p>{number}</p>
       </div>
     </div>
   );
